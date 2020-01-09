dd2b303ac3cca845e580c6c950cacb1650583493
weixiaopu11 <5358807+weixiaopu11@user.noreply.gitee.com>
2020-01-09 16:44:13 +0800

    小修改

--- api/router/adminUserRouter.js
+++ api/router/adminUserRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router =express.Router()
-const jwt = require('../../utils/jwt')
+const jwt = require('../utils/jwt')
 const adminModel = require('../db/model/personModel')
 router.post('/login',(req,res)=>{
   let {account,pwd} = req.body 
